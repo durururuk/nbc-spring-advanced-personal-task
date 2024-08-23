73606e5d3b7a8cd10107da598d5da5dcd8528dd3
durururuk <zkdltms0913@gmail.com>
2024-08-23 19:24:18 +0900

    git init

---
+++ src/main/java/com/sparta/nbcspringadvancedpersonaltask/NbcSpringAdvancedPersonalTaskApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.nbcspringadvancedpersonaltask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NbcSpringAdvancedPersonalTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NbcSpringAdvancedPersonalTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/nbcspringadvancedpersonaltask/NbcSpringAdvancedPersonalTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.nbcspringadvancedpersonaltask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NbcSpringAdvancedPersonalTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
